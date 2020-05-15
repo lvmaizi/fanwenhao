7f3c7f2b4054474930102363ef737b1c71395f8a
fanwenhao <wenhao.fan@yaoxinhd.com>
2020-05-15 14:00:56 +0800

    add

--- src/main/java/com/example/fanwwenhao/arithmetic/leedCode/KMP.java
+++ src/main/java/com/example/fanwwenhao/arithmetic/leedCode/KMP.java
@@ -16,55 +16,49 @@ import java.util.Date;
  */
 public class KMP {
     //PMT
-    static int[] getKMP(char[] p){
-        int[] kmp = new int[p.length];
-        int i=1,j=0;
-        while (i<p.length){
-            if (p[i]==p[j]){
-                kmp[i]=j+1;
-                i++;
-                j++;
+    static int[] getPMT(char[] p){
+        int[] next = new int[p.length];
+        //递推解决：求next[i] 假设0到i-1均成功求出
+        next[0] = -1;
+        int k = -1;
+        int j = 0;
+        while (j<p.length-1){
+            if(k==-1 || p[j]==p[k]){
+                next[++j] = ++k;
             }else {
-                i++;
-                j=0;
+                k= next[k];
             }
         }
-        return kmp;
+        return next;
     }
     //是否匹配成功
-    static int kmp(char[] s,char[] p,int [] pmt){
-        if (p.length==s.length && p.length ==0)return 0;
+    static int kmp(char[] s,char[] p,int [] next){
         int i=0,j=0;
-        while (i<s.length){
-            //if(j==p.length-1)return true;
-            if (s[i]==p[j]){
-                if(j==p.length-1)return i-j;
+        while (i<s.length && j<p.length){
+            if(j==-1 || s[i] == p[j]){
+                if(j == p.length-1)return i-j;
                 i++;
                 j++;
             }else {
-                if(j>0)
-                    j=pmt[j-1];
-                else {j=0;i++;}
-
+                j=next[j];
             }
         }
         return -1;
     }
-    public int strStr(String haystack, String needle) {
-        int[] kmp = getKMP(needle.toCharArray());
+    public static int strStr(String haystack, String needle) {
+        if(needle.length()==0)return 0;
+        if (0==haystack.length() || needle.length() ==0)return -1;
+        int[] kmp = getPMT(needle.toCharArray());
         return kmp(haystack.toCharArray(), needle.toCharArray(), kmp);
 
     }
     public static void main(String[] args) throws ParseException {
-//        char[] s = {'a','b','a','b','a','b','a','b','c','a'};
-//        char[] p = {'a','b','a','b','a','b','c','a'};
-
-        StringBuilder buf = new StringBuilder();
-        buf.append(":");
-        System.out.println(buf.toString());
-        char[] s = {};
-        char[] p = {};
-        int[] kmp = getKMP(p);
-        System.out.println(kmp(s, p, kmp));
+        char[] s = {'a','b','a','b','a','b','a','b','c','a'};
+        char[] p = {'a','b','a','b','a','b','c','a'};
+//        char[] s = {'h','e','l','l','o'};
+//        char[] p = {'l','l'};
+//        int[] kmp = getPMT(p);
+//        System.out.println(kmp(s, p, kmp));
+        System.out.println(strStr(String.valueOf(s),String.valueOf(p)));
     }
 }
---
+++ src/main/java/com/example/fanwwenhao/arithmetic/leedCode/_322_.java
@@ -0,0 +1,18 @@
+package com.example.fanwwenhao.arithmetic.leedCode;
+
+/**
+ * @Date 2020/5/15 11:37
+ * @Version 1.0
+ */
+public class _322_ {
+    public int coinChange(int[] coins, int amount) {
+
+        return -1;
+    }
+    //递归求得硬币个数
+    public int method(int[] coins, int amount, int length, int history){
+        if(length<0 || amount<0)return -1;
+        int val = method(coins,amount-coins[length],length-1,history);
+        return 0;
+    }
+}
